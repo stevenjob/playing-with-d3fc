2924ac8f15c1a6c0ad37c9d5d6e042c681648e99
stevenjob <steven_job@live.com>
2015-10-14 09:58:01 +0100

    server fix

---
+++ web/testi.js
@@ -0,0 +1,21 @@
+var data = fc.data.random.financial()(50);
+
+var chart = fc.chart.linearTimeSeries()
+    .xDomain(fc.util.extent(data, 'date'))
+    .yDomain(fc.util.extent(data, ['high', 'low']));
+
+var gridlines = fc.annotation.gridline();
+var candlestick = fc.series.candlestick();
+
+var multi = fc.series.multi()
+    .series([gridlines, candlestick]);
+chart.plotArea(multi);
+
+d3.select('#chart')
+    .append('svg')
+    .style({
+        height: '250px',
+        width: '600px'
+    })
+    .datum(data)
+    .call(chart);
