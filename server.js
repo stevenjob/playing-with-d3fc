03c07da11acdf1ff134c3c87e2fb0d8f5ce1a4ec
stevenjob <steven_job@live.com>
2015-10-14 09:41:03 +0100

    server fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var initDb = require("./server/db/initialise-db");
+//var initDb = require("./server/db/initialise-db");
 var server = require("./server/server");
 var express = require("express");
 
-initDb(function(db) {
-    server(process.env.PORT || 8080, db, {}, function() {
+//initDb(function(db) {
+    server(process.env.PORT || 8080, function() {
         console.log("Server listening");
     });
-});
+//});
