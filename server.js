a751ff1ea5083b85d1c41e007e77b239752a9508
jdhodges431 <jdhodges431@gmail.com>
2015-10-14 09:57:17 +0100

    Working

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ app = require("express")();
 
 app.use(express.static("public"));
 
+app.get("", function (req, res) {
+    res.sendFile(__dirname + "/web/index.html");
+});
+
 app.get("/hello", function(req, res) {
     res.send("Hello");
+});
+
+app.get("/steven", function(req, res) {
+    res.send("steven");
 });
 
 app.listen(8080);
